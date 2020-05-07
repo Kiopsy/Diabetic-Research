0dcb3c0b95e5c0f41a8bef2bdd97cdc217067a4c
Captaincow285 <Captaincow285@gmail.com>
2020-05-07 18:12:20 -0400

    Finished counting days

--- src/main/java/backend/utility/splitObjects.java
+++ src/main/java/backend/utility/splitObjects.java
@@ -7,12 +7,47 @@ import java.util.ArrayList;
 
 public class splitObjects {
 
+    private ArrayList<String> dayList = new ArrayList<String>();
+    private int[] glucose;
+    private String[] glucoseTime;
+    private int[] carbs;
+    private String[] carbsTime;
+    private double[] fastActingDosage;
+    private String[] fastActingTime;
+    private double[] longActingDosage;
+    private String[] longActingTime;
+    private String[] exerciseStartTimes;
+    private String[] exerciseDuration;
+
     public ArrayList<Day> splitObjectTransfer(ObjectTransfer object)
     {
+        //Extract from Objectransfer class
+        glucose = object.getGlucose();
+        glucoseTime = object.getGlucoseTime();
+        carbs = object.getCarbs();
+        carbsTime = object.getCarbsTime();
+
+        int dayCount = 0;
+        String previousDate = "";
+        String currentDate;
+
         //Check how many days
+        for(int i = 0; i < glucoseTime.length; i++)
+        {
+            currentDate = glucoseTime[i].substring(0, 10);
+            if(!currentDate.equals(previousDate))
+            {
+                dayCount++;
+            }
+            dayList.add(currentDate);
+            previousDate = currentDate;
+        }
+        ArrayList<Day> returnObject = new ArrayList<Day>(dayCount);
+
         //Create day array to match
         //Split Glucose into days
         //Repeat process for every other type
+        //Create Day objects
         //Return Arraylist of days
     }
 
