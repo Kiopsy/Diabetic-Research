6a74336191027f5411eee86cb3742694ca26ac07
Kiopsy <kiopsyhd@gmail.com>
2020-05-17 15:54:43 -0400

    changes within Main method

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,10 @@
 import backend.objects.*;
-import backend.utility.Input;
+//import backend.utility.Input;
+import backend.utility.Output;
 import gui.initialGUI;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class main {
 
@@ -17,7 +19,29 @@ public class main {
 
         String fileDirectory = output[1];
         System.out.println(fileDirectory);
-        //contains all parts from the input CSV (ex: 30 days worth of values specific to each array)
+        //use time for glucoseTime,carbTime, exerciseStartTime, FATime, LAtime
+        String[] time = new String[]{"12:45"};
+        int[] glucose = new int[]{123};
+        int[] carb = new int[]{10};
+        double[] dosage = new double[]{19.0};
+        String[] exerciseDuration = new String[]{"00:30:00"};
+
+        Glucose g = new Glucose(time, glucose);
+        Carbs c = new Carbs(time, carb);
+        Exercise e = new Exercise(time, exerciseDuration);
+        FastActingInsulin fa = new FastActingInsulin(time, dosage);
+        LongActingInsulin la = new LongActingInsulin(time, dosage);
+
+        String date = "2020-05-17";
+        Day exampleDay = new Day(g.getGlucoseArr(), c.getCarbsArr(), e.getExerciseArr(), fa.getInjectionsArr(), la.getInjectionsArr(), date);
+
+        ArrayList<Day> example = new ArrayList<Day>();
+        example.add(exampleDay);
+
+        Output.outputWriter(example, fileDirectory);
+
+	    /*
+	    //contains all parts from the input CSV (ex: 30 days worth of values specific to each array)
         // .parseCSV does not work
         ObjectTransfer readCSV = Input.parseCSV(fileAddress);
 
@@ -30,6 +54,7 @@ public class main {
         Exercise e = new Exercise(readCSV.getExerciseStartTimes(), readCSV.getExerciseDuration());
         FastActingInsulin fa = new FastActingInsulin(readCSV.getFastActingTime(), readCSV.getFastActingDosage());
         LongActingInsulin la = new LongActingInsulin(readCSV.getLongActingTime(), readCSV.getLongActingDosage());
+	    */
 
         program.closeGUI();
 
