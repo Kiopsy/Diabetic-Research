4932d82b6ca1614179aaa0ba47d0e768adba0628
Kiopsy <47225453+Kiopsy@users.noreply.github.com>
2020-07-16 10:15:16 -0400

    Work on GUI.fxml

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,17 +2,29 @@ import backend.objects.Day;
 import backend.utility.*;
 import gui.*;
 
-
 import java.util.ArrayList;
 
-public class main {
+public class main { /* extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+         Parent root = FXMLLoader.load(getClass().getResource("gui.fxml.GUI.fxml"));
+         primaryStage.setTitle("Hello World");
+         primaryStage.setScene(new Scene(root, 300, 275));
+         primaryStage.show();
+    }
+    */
 
 	public static void main(String[] args) {
+
+        //launch(args);
 
 	    //Loads initialGUI
         //Grabs the .CSV target file (targetCSV) to write to from initialGUI
 
         initialGUI program = new initialGUI();
+
         String[] output = program.GUI();
         String fileAddress = output[0];
         String fileDirectory = output[1];
