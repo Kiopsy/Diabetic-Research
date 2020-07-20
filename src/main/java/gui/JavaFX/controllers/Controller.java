6c6a41b52103b82e57b430534e61042561f35b0f
Capta <Captaincow285@gmail.com>
2020-07-20 12:03:56 -0400

    More FXML bindings

--- src/main/java/gui/JavaFX/controllers/Controller.java
+++ src/main/java/gui/JavaFX/controllers/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -25,24 +26,35 @@ public class Controller implements Initializable {
     @FXML
     private VBox inputPageVBox;
     @FXML
-    private VBox homePageVBox;
+    private VBox analysisPageVBox;
+
+    @FXML
+    private TextField inputCSVPath;
+    @FXML
+    private TextField outputCSVPath;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-
+        homePageVBox.toFront();
     }
 
     public void homeButtonClicked(ActionEvent actionEvent) {
-
+        homePageVBox.toFront();
     }
 
     public void inputButtonClicked(ActionEvent actionEvent) {
+        inputPageVBox.toFront();
     }
 
     public void analysisButtonClicked(ActionEvent actionEvent) {
+        analysisPageVBox.toFront();
     }
 
     public void aboutButtonClicked(ActionEvent actionEvent) {
+        //TODO: Add the aboutPageBox when it's implemented
     }
+
+    //TODO: Add ActionListeners for the file path selection
+
+    //TODO: Add ActionListener for Update
 }
