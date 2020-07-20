736e7f20842f19e344afd5f7496b250f1ca25669
Capta <Captaincow285@gmail.com>
2020-07-20 11:53:33 -0400

    Worked on actions

--- src/main/java/gui/JavaFX/controllers/Controller.java
+++ src/main/java/gui/JavaFX/controllers/Controller.java
@@ -1,15 +1,48 @@
 package gui.JavaFX.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    @FXML
+    private Button homeBtn;
+    @FXML
+    private Button inputBtn;
+    @FXML
+    private Button analysisBtn;
+    @FXML
+    private Button aboutBtn;
+
+    @FXML
+    private VBox homePageVBox;
+    @FXML
+    private VBox inputPageVBox;
+    @FXML
+    private VBox homePageVBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+
+    }
+
+    public void homeButtonClicked(ActionEvent actionEvent) {
+
+    }
+
+    public void inputButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void analysisButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void aboutButtonClicked(ActionEvent actionEvent) {
     }
 }
